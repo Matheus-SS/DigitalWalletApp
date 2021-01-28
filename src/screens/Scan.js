92629407a5c6561b04e4f5e04848388a1bb6c26d
Matheus Santos <matheus_sa_santos@hotmail.com>
2021-01-27 23:47:06 -0300

    first commit

---
+++ src/screens/Scan.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  View, 
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Image,
+  TextInput,
+  Modal,
+  FlatList,
+  KeyboardAvoidingView,
+  ScrollView
+} from 'react-native';
+
+const Scan = () => {
+  return (
+    <View>
+      <Text>SCAN</Text>
+    </View>
+  )
+};
+
+export default Scan;
